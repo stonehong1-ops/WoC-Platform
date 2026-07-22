234a0ed0353f7ed895167d381d63916dafece30c
Stone <stone@example.com>
2026-07-23 04:42:02 +0900

    fix: move Firebase to Xcode project-level SPM dependency (survives cap sync)

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -2,8 +2,7 @@
 import PackageDescription
 
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
-// ⚠️ Firebase 의존성은 Push Notifications를 위해 수동 추가됨
-// npx cap sync ios 실행 후 Firebase 의존성이 제거되면 다시 추가 필요
+// Firebase는 project.pbxproj에서 XCRemoteSwiftPackageReference로 관리됨
 let package = Package(
     name: "CapApp-SPM",
     platforms: [.iOS(.v15)],
@@ -14,9 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1"),
-        .package(name: "CapacitorApp", path: "..\\..\\..\\node_modules\\@capacitor\\app"),
-        .package(name: "CapacitorPushNotifications", path: "..\\..\\..\\node_modules\\@capacitor\\push-notifications"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0")
+        .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
+        .package(name: "CapacitorPushNotifications", path: "../../../node_modules/@capacitor/push-notifications")
     ],
     targets: [
         .target(
@@ -25,9 +23,7 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
-                .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications"),
-                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+                .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications")
             ]
         )
     ]
